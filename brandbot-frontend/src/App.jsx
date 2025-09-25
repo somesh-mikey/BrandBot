e2b152194df67d58bcc7fefacdf08a76b5eb5756
Somesh Das <someshdasbb07@gmail.com>
2025-09-25 20:12:16 +0530

    Updated Routes

--- brandbot-frontend/src/App.jsx
+++ brandbot-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage";
-import ClientDashboard from "./components/ClientDashboard";
-import AdminDashboard from "./components/AdminDashboard";
+import Dashboard from "./components/dashboard";
 import ContentHistory from "./components/content_history";
 import Pricing from "./components/pricing";
 import Profile from "./components/profile";
+import LandingPage from "./components/LandingPage";
+import AdminDashboard from "./components/AdminDashboard";
+import ClientDashboard from "./components/ClientDashboard";
 import "./App.css";
 
 function App() {
@@ -13,11 +14,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/client" element={<ClientDashboard />} />
-        <Route path="/client/content-history" element={<ContentHistory />} />
-        <Route path="/client/pricing" element={<Pricing />} />
-        <Route path="/client/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/content-history" element={<ContentHistory />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/client" element={<ClientDashboard />} />
+        {/* Add other routes as needed */}
       </Routes>
     </Router>
   );
